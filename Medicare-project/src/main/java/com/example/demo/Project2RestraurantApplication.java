aaea7d3d83b8cbe3a6a27c6c0350eea2483703e6
Sreelakshmip-simplilearn <119971936+Sreelakshmip-simplilearn@users.noreply.github.com>
2023-08-20 20:22:04 +0530

    Update Medicare-project

--- Medicare-project/src/main/java/com/example/demo/Project2RestraurantApplication.java
+++ Medicare-project/src/main/java/com/example/demo/Project2RestraurantApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project2RestraurantApplication {
+public class Medicare-project {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project2RestraurantApplication.class, args);
+		SpringApplication.run(Medicare-projectApplication.class, args);
 	}
 
 }
