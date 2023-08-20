a8b07103bd594eb22ec206f885cfbcabe310c7ed
Sreelakshmip-simplilearn <119971936+Sreelakshmip-simplilearn@users.noreply.github.com>
2023-08-20 20:24:38 +0530

    Delete Medicare-project directory

--- Medicare-project/src/main/java/com/example/demo/Dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Customer;
-
-public interface CustomerDao extends JpaRepository<Customer,Integer>{
-
-}
--- Medicare-project/src/main/java/com/example/demo/Dao/ListofItemsDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Listofitems;
-
-public interface ListofItemsDao extends JpaRepository<Listofitems,String> {
-
-}
